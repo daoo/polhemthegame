f66d9c11943026e7ca7422d6028b993e282be8ef
daoo <daoo314@gmail.com>
2011-08-25 12:03:06 +0200

    Format

--- src/credits/Credits.java
+++ src/credits/Credits.java
@@ -5,16 +5,44 @@
 package credits;
 
 public class Credits {
-  private static String  daniel      = "Daniel Oom";
-  private static String  niklas      = "Niklas Johansson";
-  public static String[] CreditsText = new String[] {
-                                     "big:Polhem The Game",
-                                     "big:An Mangoo Entertainment Product",
-                                     // "img:misc/mangoo",
-      "", "big:Lead Programmer", daniel, "", "big:Programmer", niklas, "",
-      "big:Lead Designer", niklas, "", "big:Designer", daniel, "",
-      "big:Special Effects", niklas, "", "big:Story", niklas, daniel, "",
-      "big:Level Design", niklas, daniel, "", "big:Special Thanks To",
-      "Carl Egeberg", "Marcus \"Kalle\" Kalander", "Eric \"Generalen\" McNabb",
-      "", "", "", "big:Thanks for playing!", "" };
+  private static final String  daniel      = "Daniel Oom";
+  private static final String  niklas      = "Niklas Johansson";
+  public static final String[] CreditsText = new String[] {
+      "big:Polhem The Game",
+      "big:An Mangoo Entertainment Product",
+      // "img:misc/mangoo",
+      "",
+      "big:Lead Programmer",
+      daniel,
+      "",
+      "big:Programmer",
+      niklas,
+      "",
+      "big:Lead Designer",
+      niklas,
+      "",
+      "big:Designer",
+      daniel,
+      "",
+      "big:Special Effects",
+      niklas,
+      "",
+      "big:Story",
+      niklas,
+      daniel,
+      "",
+      "big:Level Design",
+      niklas,
+      daniel,
+      "",
+      "big:Special Thanks To",
+      "Carl Egeberg",
+      "Marcus \"Kalle\" Kalander",
+      "Eric \"Generalen\" McNabb",
+      "",
+      "",
+      "",
+      "big:Thanks for playing!",
+      ""
+  };
 }
