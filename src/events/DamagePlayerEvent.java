c0ef116fa74f5d5e43601af56e8653dce72572a1
daoo <daoo314@gmail.com>
2011-09-05 10:12:18 +0200

    Implement damage player event

--- src/events/DamagePlayerEvent.java
+++ src/events/DamagePlayerEvent.java
@@ -1,10 +1,18 @@
 package events;
 
+import entities.Creep;
+import entities.Player;
+import entities.groups.Entities;
 import entities.interfaces.IObject;
 
 public class DamagePlayerEvent implements IEvent {
   @Override
   public void execute(IObject sender, IEventArgs args) {
     ObjectEventArgs eargs = (ObjectEventArgs) args;
+    float dmg = ((Creep)eargs.getObject()).getDamage();
+
+    for (final IObject e : eargs.getWorld().get(Entities.PLAYER)) {
+      ((Player) e).damage(dmg);
+    }
   }
 }
