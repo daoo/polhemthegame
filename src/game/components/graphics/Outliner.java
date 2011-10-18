09d97478afa1351e755a8748b0d9e440fa7a1659
daoo <daoo314@gmail.com>
2011-10-18 18:16:18 +0200

    Improve the outliner

--- src/game/components/graphics/Outliner.java
+++ src/game/components/graphics/Outliner.java
@@ -12,16 +12,19 @@ import org.newdawn.slick.Color;
 import org.newdawn.slick.Graphics;
 
 public class Outliner implements IRenderComponent {
-  private static final Color FIRST = Color.red;
+  private static final Color FIRST  = Color.red;
   private static final Color SECOND = Color.green;
+  private static final Color THIRD  = Color.blue;
 
   private IEntity owner;
   private Movement movement;
 
   private final boolean outlineNext;
+  private final boolean linesToNext;
 
-  public Outliner(boolean outlineNext) {
+  public Outliner(boolean outlineNext, boolean lineToNext) {
     this.outlineNext = outlineNext;
+    this.linesToNext = lineToNext;
   }
 
   @Override
@@ -42,9 +45,28 @@ public class Outliner implements IRenderComponent {
     g.setColor(FIRST);
     g.drawRect(0, 0, body.getWidth(), body.getHeight());
 
-    if (outlineNext && movement != null) {
-      g.setColor(SECOND);
-      g.drawRect(movement.getVelocity().x, movement.getVelocity().y, body.getWidth(), body.getHeight());
+    if (movement != null) {
+      float dx = movement.getVelocity().x;
+      float dy = movement.getVelocity().y;
+
+      if (outlineNext) {
+        g.setColor(SECOND);
+        g.drawRect(dx, dy, body.getWidth(), body.getHeight());
+      }
+
+      if (linesToNext) {
+        g.setColor(THIRD);
+
+        float x1 = 0;
+        float y1 = 0;
+        float x2 = body.getWidth();
+        float y2 = body.getHeight();
+
+        g.drawLine(x1, y1, x1 + dx, y1 + dy);
+        g.drawLine(x2, y1, x2 + dx, y1 + dy);
+        g.drawLine(x2, y2, x2 + dx, y2 + dy);
+        g.drawLine(x1, y2, x1 + dx, y2 + dy);
+      }
     }
     g.setColor(tmp);
   }
