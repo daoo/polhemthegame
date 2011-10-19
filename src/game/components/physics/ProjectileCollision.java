93c2132231ab8d66dc5c2a4fd1bcad1854948ac6
daoo <daoo314@gmail.com>
2011-10-19 09:40:34 +0200

    No collisions if dead

--- src/game/components/physics/ProjectileCollision.java
+++ src/game/components/physics/ProjectileCollision.java
@@ -16,27 +16,34 @@ public class ProjectileCollision implements ILogicComponent {
   private Damage damage;
   private Movement movement;
 
+  private boolean enableCollisions;
+
   public ProjectileCollision() {
-    // Do nothing
+    enableCollisions = true;
   }
 
   @Override
   public void update(final GameTime time) {
-    // Check for collisions with units
-    final Rectangle a = owner.getBody();
-    for (final IEntity e2 : owner.getWorld().getUnits()) {
-      if (CollisionHelper.sweepCollisionTest(a, movement.getVelocity(), e2.getBody(), time.getFrameLength())) {
-        // FIXME: If the projectile can hit multiple targets and is sufficently slow,
-        //        it might hit the same target multiple times.
-        e2.sendMessage(ComponentMessage.DAMAGE, damage);
-        owner.sendMessage(ComponentMessage.DAMAGE, DAMAGE_ONE);
+    if (enableCollisions) {
+      // Check for collisions with units
+      final Rectangle a = owner.getBody();
+      for (final IEntity e2 : owner.getWorld().getUnits()) {
+        if (CollisionHelper.sweepCollisionTest(
+              a, movement.getVelocity(), e2.getBody(), time.getFrameLength())) {
+          // FIXME: If the projectile can hit multiple targets and is sufficently slow,
+          //        it might hit the same target multiple times.
+          e2.sendMessage(ComponentMessage.DAMAGE, damage);
+          owner.sendMessage(ComponentMessage.DAMAGE, DAMAGE_ONE);
+        }
       }
     }
   }
 
   @Override
-  public void reciveMessage(final ComponentMessage message, final Object args) {
-    // Do nothing
+  public void reciveMessage(ComponentMessage message, Object args) {
+    if (message == ComponentMessage.KILL) {
+      enableCollisions = false;
+    }
   }
 
   @Override
@@ -46,8 +53,8 @@ public class ProjectileCollision implements ILogicComponent {
 
   @Override
   public void setOwner(final IEntity owner) {
-    this.owner = owner;
-    this.damage = (Damage) owner.getComponent(ComponentType.DAMAGE);
+    this.owner    = owner;
+    this.damage   = (Damage) owner.getComponent(ComponentType.DAMAGE);
     this.movement = (Movement) owner.getComponent(ComponentType.MOVEMENT);
   }
 }
