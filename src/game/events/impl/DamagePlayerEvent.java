566492a79e8d7717cb4f4d0e45b4f2f6a783f027
daoo <daoo314@gmail.com>
2011-12-12 09:27:12 +0100

    Fix player damage

--- src/game/events/impl/DamagePlayerEvent.java
+++ src/game/events/impl/DamagePlayerEvent.java
@@ -8,15 +8,20 @@ import game.components.ComponentMessage;
 import game.entities.EntityType;
 import game.entities.IEntity;
 import game.events.IEvent;
+import game.pods.Damage;
 
 public class DamagePlayerEvent implements IEvent<ObjectEventArgs> {
+  private final Damage damage;
+
+  public DamagePlayerEvent() {
+    this.damage = new Damage(null, 10); // FIXME: Magic number
+  }
+
   @Override
   public void execute(IEntity sender, ObjectEventArgs args) {
     if (args.getObject().getType() == EntityType.CREEP) {
-      float dmg = 10; // FIXME: Magic number
-
       for (IEntity e : args.getWorld().get(EntityType.PLAYER)) {
-        e.sendMessage(ComponentMessage.DAMAGE, Float.valueOf(dmg));
+        e.sendMessage(ComponentMessage.DAMAGE, damage);
       }
     }
   }
