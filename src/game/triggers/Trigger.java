8db18e41881a448422993977153219fbc135e802
daoo <daoo314@gmail.com>
2011-12-20 19:12:01 +0100

    Add comments

--- src/game/triggers/Trigger.java
+++ src/game/triggers/Trigger.java
@@ -9,6 +9,10 @@ import game.world.World;
 
 import java.util.LinkedList;
 
+/**
+ * A trigger that can be looped. More specifically a set of effects that can be
+ * executed when some conditions are met.
+ */
 public class Trigger implements ITrigger {
   private final LinkedList<ICondition> conditions;
   private final LinkedList<IEffect> effects;
@@ -18,6 +22,10 @@ public class Trigger implements ITrigger {
 
   private World world;
 
+  /**
+   * Create a new trigger with no conditions or effects.
+   * @param loop specifies if this triggers should loop
+   */
   public Trigger(boolean loop) {
     this.loop = loop;
     runAgain = true;
@@ -26,21 +34,38 @@ public class Trigger implements ITrigger {
     effects = new LinkedList<>();
   }
 
+  /**
+   * Sets the world in which the conditions should be evaluated and the effects
+   * exectued.
+   * @param world the world to use, can not be null
+   */
   @Override
   public void setWorld(World world) {
+    assert world != null;
+
     this.world = world;
   }
 
+  /**
+   * Tells if this trigger will run it's effects again.
+   * @return true if this is a looping trigger, if it isn't it will return true
+   *         or false depending on if the trigger have been executed or not.
+   */
   @Override
   public boolean runAgain() {
     return runAgain;
   }
 
+  /**
+   * Run the effects if the conditions are true. Also takes looping into
+   * account.
+   * @param time the game time to use for execution and evaluation
+   */
   @Override
   public void update(GameTime time) {
     if (runAgain) {
       if (checkConditions(time)) {
-        doActions(time);
+        execute(time);
 
         if (!loop) {
           runAgain = false;
@@ -49,6 +74,13 @@ public class Trigger implements ITrigger {
     }
   }
 
+  /**
+   * Checks if the conditions are met.
+   * Conditions are met if the list of conditions is non-empty and ALL
+   * conditions evaluate to true.
+   * @param time the game time to use when evaluating conditions
+   * @return true if all conditions are met or false otherwise
+   */
   private boolean checkConditions(GameTime time) {
     if (conditions.isEmpty()) {
       return false;
@@ -63,19 +95,35 @@ public class Trigger implements ITrigger {
     return true;
   }
 
-  private void doActions(GameTime time) {
+  /**
+   * Execute all effects with a specific game time.
+   * @param time the time to use
+   */
+  private void execute(GameTime time) {
     for (IEffect effect : effects) {
       effect.execute(time, world);
     }
   }
 
+  /**
+   * Adds a condition to the trigger.
+   * @param condition the condition to add, can not be null
+   */
   @Override
   public void addCondition(ICondition condition) {
+    assert condition != null;
+
     conditions.add(condition);
   }
 
+  /**
+   * Adds an effect to the trigger.
+   * @param effect the effect to add, can not be null
+   */
   @Override
   public void addEffect(IEffect effect) {
+    assert effect != null;
+
     effects.add(effect);
   }
 }
