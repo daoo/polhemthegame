010b3a0675dc8fa665ceaccbe07bf7269bf6d728
daoo <daoo314@gmail.com>
2012-01-14 15:22:15 +0100

    Formatting and upped version!

--- src/main/Launcher.java
+++ src/main/Launcher.java
@@ -20,12 +20,14 @@ import states.StateManager;
 
 public class Launcher extends BasicGame {
   public static final String NAME    = "PolhemTheGame";
-  public static final String VERSION = "1.0";
+  public static final String VERSION = "2.0";
 
-  public static final int       WIDTH      = 1024;
-  public static final int       HEIGHT     = 768;
-  public static final boolean   FULLSCREEN = false;
-  public static final int       MAX_FPS    = 60;
+  public static final int WIDTH   = 1024;
+  public static final int HEIGHT  = 768;
+
+  public static final int MAX_FPS = 60;
+
+  public static final boolean FULLSCREEN = false;
 
   private StateManager stateGame;
 
