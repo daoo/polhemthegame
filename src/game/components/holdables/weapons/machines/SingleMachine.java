599fb09e1094740da372c5f1de5d9354a6977153
daoo <daoo314@gmail.com>
2012-02-08 08:46:39 +0100

    Add copyright comments

--- src/game/components/holdables/weapons/machines/SingleMachine.java
+++ src/game/components/holdables/weapons/machines/SingleMachine.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2009-2011 Daniel Oom, see license.txt for more info.
+ */
+
 package game.components.holdables.weapons.machines;
 
 import game.components.graphics.animations.Idle;
