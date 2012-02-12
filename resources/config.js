4dd457f52d54bf125be3bcb8dc3d99bab0b83143
daoo <daoo314@gmail.com>
2012-02-12 12:01:01 +0100

    Refactor directory structure

---
+++ resources/config.js
@@ -0,0 +1,16 @@
+{ player1:
+  { walkUp: 17
+  , walkDown: 31
+  , walkLeft: 30
+  , walkRight: 32
+  , fire: 57
+  , nextWeapon: 16
+  , buy: 18 }
+, player2:
+  { walkUp: 200
+  , walkDown: 208
+  , walkLeft: 203
+  , walkRight: 205
+  , fire: 28
+  , nextWeapon: 54
+  , buy: 157 } }
