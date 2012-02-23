24a57d7892279ee58b508ea1fe618f290b5e2211
daoo <daoo314@gmail.com>
2012-02-23 20:38:10 +0100

    Remove test level from campaign

--- resources/campaigns/polhem.js
+++ resources/campaigns/polhem.js
@@ -2,7 +2,6 @@
 , constraints: [ 0, 100, 0, 0 ]
 , background: "textures/levels/polhem.png"
 , levels:
-  [ "test"
-  , "defend_what_matters_most"
+  [ "defend_what_matters_most"
   , "ytc"
   , "bobby" ] }
