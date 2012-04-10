45724fad146556af76680768fbb53087a3d179d0
daoo <daoo314@gmail.com>
2012-04-10 15:20:49 +0200

    Remove test level

--- resources/levels/test.js
+++
@@ -1,7 +0,0 @@
-{ level: "Test"
-, loading: "textures/text/donteventrytogetbackin.png"
-, completed: "textures/text/levelcomplete.png"
-, preBossImage: "textures/text/donteventrytogetbackin.png"
-, boss: "datateknikern"
-, creeps: [] }
-
